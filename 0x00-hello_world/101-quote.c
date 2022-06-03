0f05e771facd528e375393518eda701bbc9716b3
Akacha <samchijioke16@gmail.com>
2022-06-03 04:15:24 +0100

    Last Task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Print to stderr
+ *
+ * Description: Print to stderr without printf() and puts()
+ * Return: Returns an integer(0)
+ */
+int main (void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (0);
+}
