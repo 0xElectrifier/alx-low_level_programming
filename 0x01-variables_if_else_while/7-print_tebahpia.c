41ee35114d8ba8e7b82b8cb8443e569678b6128c
Akacha <samchijioke16@gmail.com>
2022-06-04 02:21:02 +0100

    Print tebahpia

---
+++ 0x01-variables_if_else_while/7-print_tebahpia.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Function that prints lowwercase alphabet in reverse
+ *
+ * Description: Prints lowercase alphabet in reverse with putchar()
+ * Return: Returns integer(0)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
+	return (0);
+}
