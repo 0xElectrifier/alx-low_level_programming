cb8baf895c99338ea5ed1760db153de9e704fa4b
Akacha <samchijioke16@gmail.com>
2022-06-07 18:42:40 +0100

    x10 Lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print alphabets
+ * @void: No arguement passed
+ *
+ * Description: Print 10 times the letter of lowercase alphabet
+ * Return: nothing
+ */
+void print_alphabet_x10(void)
+{
+	char lowercase = 'a';
+	int i = 0;
+
+	while (i <= 9)
+	{
+		while (lowercase <= 'z')
+		{
+			_putchar(lowercase);
+			lowercase++;
+		}
+		i++;
+	}
+	_putchar('\n');
+}
