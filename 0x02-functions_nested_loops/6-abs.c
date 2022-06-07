0a8e43034f8c466352e88fc34a81c2c7c0b57011
Akacha <samchijioke16@gmail.com>
2022-06-07 20:15:39 +0100

    Commutes absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - Computes absolute value of an integer
+ * 
+ * Description: Computes absolute value of an integer
+ * Returns: Always 0.
+ */
+int _abs(int)
+{
+	abs(j);
+}
