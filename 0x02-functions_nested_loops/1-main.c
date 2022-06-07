8bc25cd0be1347a1c0990f64805fb5e1e9f3f7b5
Akacha <samchijioke16@gmail.com>
2022-06-07 17:41:36 +0100

    Lowercase

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char lowercase = 'a';
+
+	while (lowercase <= 'z')
+	{
+		_putchar(lowercase);
+		lowercase++;
+	}
+	
+	return (0);
+}
