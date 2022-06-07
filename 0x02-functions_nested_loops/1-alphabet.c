97868db9fe2eef4281793f6591f90f35c1bfdabe
Akacha <samchijioke16@gmail.com>
2022-06-07 13:32:33 +0100

    First Commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Function that prints '_putchar'
+ *
+ * Description: Print '_putchar' only
+ * Return: 0 if compilation is successful
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
