aa407a02888c8baf6045718978302a02c3fa2e05
Akacha <samchijioke16@gmail.com>
2022-06-07 20:18:54 +0100

    Commutes absolute value of an integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,9 +3,9 @@
 
 /**
  * _abs - Computes absolute value of an integer
- * 
+ * @int: Takes any integer value as argument
  * Description: Computes absolute value of an integer
- * Returns: Always 0.
+ * Return: Always 0.
  */
 int _abs(int)
 {
