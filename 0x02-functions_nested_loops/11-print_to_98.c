83f4efcd337d911dc9f72c67e63b9dc7d742024a
Akacha <samchijioke16@gmail.com>
2022-06-08 03:57:11 +0100

    Print

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,12 +8,27 @@
  */
 void print_to_98(int n)
 {
-	int i;
+	int i, j;
 
-	for (i = n; i <= 98; i++)
+	if (n <= 98)
 	{
-		_putchar(i + '0');
-		_putchar(',');
-		_putchar(' ');
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
+		}
+	} else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
+
+		}
+
 	}
 }
