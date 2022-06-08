2796cbfbb389801179a30a3ff5d0a8c7622b80f0
Akacha <samchijioke16@gmail.com>
2022-06-08 03:28:30 +0100

    Loop

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,9 +10,9 @@ void times_table(void)
 {
 	int i, j, product;
 
-	for (i = 0; i <= '9'; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = '0'; j <= '9'; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			product = i * j;
 			if (j == 0)
