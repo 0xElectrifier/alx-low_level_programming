2d77d564337c099082e1cbbd9413729f0d1de9c7
Akacha <samchijioke16@gmail.com>
2022-06-08 03:33:38 +0100

    Sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Check the code
+ * @i, j: Two parameters
+ * Description: Prints things
+ * Return: Integer
+ */
+int add(int i, int j)
+{
+	int sum;
+
+	sum = i + j;
+
+	return (sum);
+}
