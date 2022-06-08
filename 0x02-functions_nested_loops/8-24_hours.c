87b975e5992e92151c642828827ac9de2b347ffd
Akacha <samchijioke16@gmail.com>
2022-06-08 02:37:26 +0100

    Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -26,6 +26,10 @@ void jack_bauer(void)
 					_putchar('\n');
 				}
 			}
+			if ((i == '2') && (j == '3') &&
+                                                (k == '5') && (l == '9'))
+                                                break;
+
 		}
 	}
 }
