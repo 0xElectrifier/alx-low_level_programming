8467b62e019814bec2f4968a22c8d73b31f0175f
Akacha <samchijioke16@gmail.com>
2022-06-08 02:14:19 +0100

    Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -18,17 +18,13 @@ void jack_bauer(void)
 			{
 				for (l = '0'; l <= '9'; l++)
 				{
-/**
- *					if ((i = '2') && (j > 3))
- *					break;
- */
 					_putchar(i);
 					_putchar(j);
 					_putchar(':');
 					_putchar(k);
 					_putchar(l);
-					if ((i = '2') && (j = '3') && 
-						(k = '5') && (l = '9'))
+					if ((i == '2') && (j == '3') &&
+						(k == '5') && (l == '9'))
 						break;
 					_putchar('\n');
 				}
