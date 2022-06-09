c78819c8a3ef1abcc05753ac2786801ccd3e3782
Akacha <samchijioke16@gmail.com>
2022-06-09 17:53:16 +0100

    Last Commit

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -20,16 +20,25 @@ void print_remaining_days(int month, int day, int year)
 			{
 				if (day >= 60)
 					day++;
+				else
+					day = day;
 			}
 		}
 		else
 		{
 			if (day >= 60)
 				day++;
+			else
+				day = day;
 		}
-	}
-	printf("Day of the year: %d\n", day);
-	printf("Remaining days: %d\n", 366 - day);
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
 
+	}
+	else
+	{
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 365 - day);
+	}
 }
 
