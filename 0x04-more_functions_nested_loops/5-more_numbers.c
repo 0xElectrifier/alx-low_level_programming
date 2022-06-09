4b59b04212d556660648f51a2e2cfa795817bde3
Akacha <samchijioke16@gmail.com>
2022-06-10 00:27:39 +0100

    1 -14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints 0 - 14, 10 times
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 11; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
