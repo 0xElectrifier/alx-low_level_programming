ce782d488a604be6f86b42318599b46f881384d3
Akacha <samchijioke16@gmail.com>
2022-06-09 22:47:25 +0100

    Is uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,9 +7,9 @@
  * Description: Uses isupper() to check
  * Return: 1 if c is uppercase and 0 otherwise
  */
-int _isupper(int c )
+int _isupper(int c)
 {
-	if(isupper(c))
+	if (isupper(c))
 		return (1);
 	else
 		return (0);
