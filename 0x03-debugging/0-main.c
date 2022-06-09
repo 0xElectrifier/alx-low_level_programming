44f8ce0c20756489ca5106bb73eea840ffc9fc9e
Akacha <samchijioke16@gmail.com>
2022-06-09 11:46:43 +0100

    Positive or Negative in main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/** main - Main function to check for a negative or positive number
+ * void: Takes no arguement
+ * Description: Takes an arguement as checks if it is positive or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
