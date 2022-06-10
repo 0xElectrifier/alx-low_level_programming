da570b0363d983c40721dfac2f41d303a3c1f22c
Akacha <samchijioke16@gmail.com>
2022-06-10 01:48:05 +0100

    Diagonal Line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,19 +10,20 @@ void print_diagonal(int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	if (n > 0)
 	{
-		if (n <= 0)
-			continue;
-		int j;
-
-		for (j = 0; j < n; j++)
+		for (i = 0; i < n; i++)
 		{
-			_putchar(' ');
+			int j;
+
+			for (j = 0; j < n; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
 		}
-		_putchar('\\');
-		_putchar('\n');
 	}
-	if (n <= 0)
+	else
 		_putchar('\n');
 }
