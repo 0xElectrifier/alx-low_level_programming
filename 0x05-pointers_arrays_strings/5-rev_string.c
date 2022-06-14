66475390b842dc9654aac3a460e6b9b43bb6193f
Akacha <samchijioke16@gmail.com>
2022-06-14 01:33:12 +0100

    Reverse String 2

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,16 +12,14 @@ void rev_string(char *s)
 	int i;
 
 	i = 0;
-	while (*s[i] != '\0')
+	while (s[i] != '\0')
 	{
 		i++;
 		continue;
 	}
-	i--;
 	while (i >= 0)
 	{
-		_putchar(*s[i]);
+		_putchar(s[i]);
 		i--;
 	}
-	_putchar('\n');
 }
