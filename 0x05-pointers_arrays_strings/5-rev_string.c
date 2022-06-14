66dc549975846c5f6967c399acb45d1ff093fef8
Akacha <samchijioke16@gmail.com>
2022-06-14 13:37:21 +0100

    Rev String

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,19 +10,19 @@
 void rev_string(char *s)
 {
 	int i, j, len;
-	char *s1;
+	char s1;
 
-	s1 = s;
 	i = 0;
 
-	while (s1[i] != '\0')
+	while (s[i] != '\0')
 		i++;
 	i--;
 	len = i;
 
-	for (j = 0; j <= len; j++)
+	for (j = 0; j <= (i / 2); j++)
 	{
-		s[j] = s1[i];
-		i--;
+		s1 = s[j];
+		s[j] = s[len];
+		s[len--] = s1;
 	}
 }
