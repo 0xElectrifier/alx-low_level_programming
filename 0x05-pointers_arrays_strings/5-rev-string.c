475ec835ebb1c1ce8a18886323523553259a4c79
Akacha <samchijioke16@gmail.com>
2022-06-14 01:16:55 +0100

    Reverse String 2

---
+++ 0x05-pointers_arrays_strings/5-rev-string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * rev-string - Reverses a string
+ * @s: Takes s as parameter
+ *
+ * Return: Nothing
+ */
+void rev_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		continue;
+	while (i >= 0)
+		_putchar(s[i]);
+	_putchar('\n');
+}
