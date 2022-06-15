8a07b6cdea9cedb6ca1d5ac1ebf0172fc3347e8f
Akacha_TermuxMobile <samchijioke16@gmail.com>
2022-06-15 13:47:12 +0100

    First Commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _strcat - Function that concatenates two strings
+ * @dest: First string to be concatenated
+ * @src: Appends src to dest
+ *
+ * Return: Pointer to the resulting string
+ */
+
