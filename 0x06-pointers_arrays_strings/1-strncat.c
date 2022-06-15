3b34718eee9cf6c7e028afc9d2830032745757a9
Akacha <samchijioke16@gmail.com>
2022-06-15 22:57:02 +0100

    Stack smashing?

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,13 +12,14 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 	char *s;
+
 	s = dest;
 
 	for (i = 0; dest[i] != '\0'; i++)
 	{
 		s[i] = dest[i];
 	}
-	
+
 	for (j = 0; j < n; j++)
 	{
 		s[i] = src[j];
