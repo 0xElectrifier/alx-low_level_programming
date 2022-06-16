51cfac329c8626244e53746302cd814cd20a1cfb
Akacha <samchijioke16@gmail.com>
2022-06-16 02:19:38 +0100

    2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,15 +10,19 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, len_dest;
+	int i, len_dest, len_src;
 
 	for (len_dest = 0; dest[len_dest] != '\0'; len_dest++)
+		continue;
+	for (len_src = 0; src[len_src] != '\0'; len_src++)
 		continue;
 
 	for (i = 0; i < n; i++)
 	{
 		if (src[i] == '\0')
-			dest[i] = '\0';
+		{	for (; i < n; i++)
+				dest[i] = '\0';
+		}
 		else
 			dest[i] = src[i];
 	}
