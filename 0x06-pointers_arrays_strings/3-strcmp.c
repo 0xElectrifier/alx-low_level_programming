ef2d80996c4344b821788f75182ae821a17e404c
Akacha <samchijioke16@gmail.com>
2022-06-16 02:58:03 +0100

    Compare two numbers

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,9 +10,16 @@
  */
 int *_strcmp(char *s1, char *s2)
 {
-	if (s1 == s2)
+	int len_s1, len_s2;
+
+	for (len_s1 = 0; s1[len_s1] != '\0'; len_s1++)
+		continue;
+	for (len_s2 = 0; s2[len_s2] != '\0'; len_s2++)
+		continue;
+
+	if (len_s1 == len_s2)
 		return (0);
-	else if (s1 < s2)
+	else if (len_s1 < len_s2)
 		return (-15);
 	else
 		return (15);
