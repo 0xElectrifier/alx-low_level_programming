948f0101785cff7b5713555a44b8aa788bed5ba3
Akacha <samchijioke16@gmail.com>
2022-06-16 02:21:00 +0100

    2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -20,7 +20,8 @@ char *_strncpy(char *dest, char *src, int n)
 	for (i = 0; i < n; i++)
 	{
 		if (src[i] == '\0')
-		{	for (; i < n; i++)
+		{
+			for (; i < n; i++)
 				dest[i] = '\0';
 		}
 		else
