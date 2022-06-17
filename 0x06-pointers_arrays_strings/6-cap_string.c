da534b6766cf4ea1521114484e8c645afc9a3e6b
Akacha <samchijioke16@gmail.com>
2022-06-17 02:37:17 +0100

    Look at the rubbish I wrote

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,20 +1,7 @@
 #include "main.h"
 
 /**
- * is_lowercase - Checks for Lowercase
- * @n: Array to loop through
- * @i: incremental loop value
- *
- * Return: 0 if false and anyother value if true
- *
-int is_lowercase(char *n, int j)
-{
-	return(n[j] >= 'A' && n[j] <= 'Z');
-}
-*/
-
-/**
- * symbol - Loops through s array to match symbol
+ * is_symbol - Loops through s array to match symbol
  * @n: Array to loop through
  * @i: incremental loop value
  * Return: 0 if false and anyother value if true
@@ -23,7 +10,7 @@ int is_symbol(char *n, int i)
 {
 	int k;
 
-	char s[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', ' ', 
+	char s[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', ' ',
 	'\t', '\n'};
 
 	for (k = 0; s[k] != '\0'; k++)
