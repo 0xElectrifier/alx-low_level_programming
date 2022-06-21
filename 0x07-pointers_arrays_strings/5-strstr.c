ed46d1cea34501a8bda1c19a8786b6ae17d59d62
Akacha <samchijioke16@gmail.com>
2022-06-21 02:37:51 +0100

    Fixed segmentation fault

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -31,6 +31,9 @@ char *_strstr(char *haystack, char *needle)
 	while (needle[len_needle] != '\0')
 		len_needle++;
 
+	if (len_needle == 0)
+		return (haystack);
+	else
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
