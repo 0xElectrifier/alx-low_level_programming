d4834390641291a2073bfb75ceafa5082453c408
Akacha <samchijioke16@gmail.com>
2022-06-21 01:55:26 +0100

    Status

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -13,3 +13,9 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
 int is_equal(char *h, char *n, unsigned int i, unsigned int j);
+
+void print_chessboard(char (*a)[8]);
+
+void print_diagsums(int *a, int size);
+
+void set_string(char **s, char *to);
