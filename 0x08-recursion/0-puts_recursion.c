9f6371b83bb60e56ddc9b5ec2f721cc7f85d6fe7
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-06-22 19:17:19 +0100

    First Commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string, followed by a new line
+ * @s: string to be printed
+ *
+ * Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(s);
+
+	if (s != '\0')
+		_puts_recursion(s + 1);
+	else
+		_putchar('\0');
+}
