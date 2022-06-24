fdb71a8da9f541ad9f5d60c10964b067159de351
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-06-24 23:57:04 +0100

    Program that prints it name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - function that prints it name
+ * @argc: number of command line arguements
+ * @argv: array containing pointers to command line arguements
+ *
+ * Return: always 0
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+ARGC vs ARGV
+
