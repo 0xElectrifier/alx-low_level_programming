b024db51b4b1f7cb80e179d662fdfb25ccc33b55
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-06-25 01:02:47 +0100

    2-args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - function that prints all arguments it receives
+ * @argc: number of arguments passed
+ * @argv: array of points to strings passed on command line
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0; argv[i] != '\0'; i++)
+		printf("%s\n", argv[i];
+
+	return (0);
+}
