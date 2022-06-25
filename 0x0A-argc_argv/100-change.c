139e6f56061b9fe3385e1f083c97b26291b047c4
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-06-25 18:52:29 +0100

    Modified 100-change file

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,38 +1,60 @@
 #include <stdio.h>
-#include <stdlib.h>
-/**
- * main - prints the minimum number of coins to make change for an amount.
- * of money.
- * @argc: number of command line arguments.
- * @argv: array that contains the program command line arguments.
- * Return: 0 - success.
- */
-int main(int argc, char *argv[])
-{
-	int cents, ncoins = 0;
+#include "main.h"
 
-	if (argc == 1 || argc > 2)
+/**
+ * main - function that multiplies two numbers
+ * @argc: number of arguments passed
+ * @argv: array of points to strings passed on command line
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int cent, change;
+
+	change = 0;
+
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	cents = atoi(argv[1]);
-
-	while (cents > 0)
+	cent = atoi(argv[1]);
+	if (cent < 0)
 	{
-		if (cents >= 25)
-			cents -= 25;
-		else if (cents >= 10)
-			cents -= 10;
-		else if (cents >= 5)
-			cents -= 5;
-		else if (cents >= 2)
-			cents -= 2;
-		else if (cents >= 1)
-			cents -= 1;
-		ncoins += 1;
+		printf("0\n");
+		return (0);
 	}
-	printf("%d\n", ncoins);
+
+	change_cent(&cent, 25, &change);
+	change_cent(&cent, 10, &change);
+	change_cent(&cent, 5, &change);
+	change_cent(&cent, 2, &change);
+	change_cent(&cent, 1, &change);
+
+	printf("%d\n", change);
+
+	return (0);
+}
+
+/**
+ * change_cent - function that counts how much change a value gives you
+ * @num: pointer to input cent value
+ * @subt: the integer subtrahend
+ * @change: the nubmer of times subt successfully subtracts num
+ *
+ * Return: alwalys 0;
+ */
+int change_cent(int *num, int subt, int *change)
+{
+	int *s_num;
+
+	s_num = num;
+	while (*s_num >= subt)
+	{
+		*s_num -= subt;
+		++*change;
+	}
 	return (0);
 }
