a932674953303f9e2ebf7c466508f401bdde5db9
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-06-27 19:03:58 +0100

    The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,15 +18,14 @@ char *_strdup(char *str)
 
 	if (i == 0)
 		return (NULL);
-	else
-	{
-		s = malloc(i * sizeof(char));
-		if (str == NULL || s == NULL)
-			return (NULL);
 
-		for (j = 0; j < i; j++)
-			s[j] = str[j];
-	}
+	s = malloc(i * sizeof(char));
+	if (str == NULL || s == NULL)
+		return (NULL);
+
+	for (j = 0; j < i; j++)
+		s[j] = str[j];
+
 
 	return (s);
 }
