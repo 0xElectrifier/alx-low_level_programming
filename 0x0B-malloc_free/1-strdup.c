831784cc8d40f8b7db081013a9de6f4d36a9d979
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-06-27 19:01:41 +0100

    The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strdup - function that returns a pointer to a newly allocated space in
+ * memory, which contains a copy of the string given as a parameter
+ * @str: str to be referenced
+ *
+ * Return: pointer to the newly allocated space
+ */
+char *_strdup(char *str)
+{
+	unsigned int i, j;
+	char *s;
+
+	i = 0;
+	while (*(str + i) != '\0')
+		i++;
+
+	if (i == 0)
+		return (NULL);
+	else
+	{
+		s = malloc(i * sizeof(char));
+		if (str == NULL || s == NULL)
+			return (NULL);
+
+		for (j = 0; j < i; j++)
+			s[j] = str[j];
+	}
+
+	return (s);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -5,3 +5,5 @@
 int _putchar(char n);
 
 char *create_array(unsigned int size, char c);
+
+char *_strdup(char *Str);
