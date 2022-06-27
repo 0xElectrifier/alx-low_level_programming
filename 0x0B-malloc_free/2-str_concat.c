af7df49f030bc6555b6097aff394e2b247b6a5db
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-06-28 00:40:17 +0100

    2.* task

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * str_concat - function that concatenates two string
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: the concatenateed string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int len_s1, len_s2, i, j;
+	char *concat;
+
+	len_s1 = 0;
+	len_s2 = 0;
+	i = 0;
+	j = 0;
+	while (s1[len_s1] != '\0')
+	{
+		if (s1 == NULL)
+			break;
+		len_s1++;
+	}
+
+	while (s2[len_s2] != '\0')
+	{
+		if (s2 == NULL)
+			break;
+		len_s2++;
+	}
+
+	concat = malloc((len_s1 + len_s2) * sizeof(char));
+	if (concat == NULL)
+		return (NULL);
+
+	if (s1 != NULL)
+	{
+		for (; i < len_s1; i++)
+			concat[i] = s1[i];
+	}
+	if (s2 == NULL)
+		concat[i] = '\0';
+	else
+	{
+		for (; j <= len_s2; j++)
+		{
+			concat[i] = s2[len_s2];
+			i++;
+		}
+	}
+
+	return (concat);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -7,3 +7,5 @@ int _putchar(char n);
 char *create_array(unsigned int size, char c);
 
 char *_strdup(char *Str);
+
+char *str_concat(char *s1, char *s2);
