3dc369fbaedcf75c107153f528918560561dc4f2
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-06-28 19:06:55 +0100

    First free_grid func push

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * free_grid - function that frees a 2 dimensional grid
+ * @grid: array to be free'd
+ * @height: amouunt of rows already allocated memory
+ *
+ * Return: grid
+ */
+void free_grid(int **grid, int height)
+{
+	int x;
+
+	for (x = height; x >= 0; x--)
+	{
+		free(grid[x]);
+	}
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -13,3 +13,5 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 
 int **free_array(int **array, int row);
+
+void free_grid(int **grid, int height);
