e21c0894881f005a719b8f4c5bb8a5dc50b407e7
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-06-28 17:53:06 +0100

    Free'd NULL arrays

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,6 +1,40 @@
 #include "main.h"
 
 /**
+ * is_positive - checks of the arguments are positive
+ * @width: first parameter
+ * @height: second parameter
+ *
+ * Return: 0 if false otherwise, 1
+ */
+int is_positive(int width, int height)
+{
+	return (width * height == 0 || width < 0 || height < 0);
+}
+
+/**
+ * free_grid - frees a 2D array if malloc fails
+ * @array: array to be freed
+ * @
+ * Return: array
+ */
+int **free_array(int **array, int row)
+{
+	int x;
+
+	if (array == NULL)
+		return (NULL);
+	for (x = row - 1; x >= 0; x--)
+	{
+		if (array[row])
+			free(array[row]);
+	}
+
+	free(array);
+
+	return (array);
+}
+/**
  * alloc_grid - function that returns a pointer to a 2 dimensional array
  * of integers
  * @width: width of array
@@ -12,13 +46,21 @@ int **alloc_grid(int width, int height)
 {
 	int **array, row, i, j;
 
-	if (width * height == 0)
+	if (is_positive(width, height))
+	{
 		return (NULL);
+	}
 
 	array = malloc(height * sizeof(int *));
 	for (row = 0; row < height; row++)
+	{
 		array[row] = malloc(width * sizeof(int));
-
+		if (array[row] == NULL)
+		{
+			free_array(array, row);
+			return (NULL);
+		}
+	}
 
 	for (i = 0; i < height; i++)
 	{
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -11,3 +11,5 @@ char *_strdup(char *Str);
 char *str_concat(char *s1, char *s2);
 
 int **alloc_grid(int width, int height);
+
+int **free_array(int **array, int row);
