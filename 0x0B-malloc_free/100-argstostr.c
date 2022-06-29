56dd85f29947493bb167308b64c5b18e2f4286cf
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-06-29 02:34:03 +0100

    Ready

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,54 +1,52 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * len_arr - loops through argv array and checks length of all characters
- * @argv: array to be looped
+ * argstostr - concatenates all the arguments of a program.
+ * @ac: argument count.
+ * @av: argument vector.
  *
- * Return: len_arg
- */
-int len_arr(int argc __attribute__((unused)), char **argv)
-{
-	int i, j, *len_arg = 0;
-
-	for (i = 0; *argv[i] != '\0'; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			*len_arg = *len_arg + 1;
-		}
-	}
-	return (*len_arg);
-}
-/**
- * argstostr - function that concatenates all the arguments of your program
- * @ac: length of array
- * @av: array of arguments to be concatenated
- *
- * Return: concatenated string
+ * Return: pointer of an array of char
  */
 char *argstostr(int ac, char **av)
 {
-	int i, j, *k = 0, len_array;
-	char *str;
+	char *aout;
+	int c, i, j, ia;
 
-	if (ac == 0 || av == NULL)
+	if (ac == 0)
 		return (NULL);
 
-	len_array = len_arr(ac, av);
-
-	str = malloc((len_array + ac) * sizeof(char));
-	if (str == NULL)
-		return (NULL);
-
-	for (i = 0; *av[i] != '\0'; i++)
+	for (c = i = 0; i < ac; i++)
 	{
+		if (av[i] == NULL)
+			return (NULL);
+
 		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			str[*k] = av[i][j];
-			k += 1;
-		}
-		str[*k] = '\n';
-		k += 1;
+			c++;
+		c++;
 	}
-	return (str);
+
+	aout = malloc((c + 1) * sizeof(char));
+
+	if (aout == NULL)
+	{
+		free(aout);
+		return (NULL);
+	}
+
+	for (i = j = ia = 0; ia < c; j++, ia++)
+	{
+		if (av[i][j] == '\0')
+		{
+			aout[ia] = '\n';
+			i++;
+			ia++;
+			j = 0;
+		}
+		if (ia < c - 1)
+			aout[ia] = av[i][j];
+	}
+	aout[ia] = '\0';
+
+	return (aout);
 }
