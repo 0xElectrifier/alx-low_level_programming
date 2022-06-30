aa9769ccdff47891a95a12d47232b95f4b6e7746
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-06-30 16:53:51 +0100

    Calloc function

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _calloc - function that allocates memory for an array using malloc
+ * @nmemb: number of elements
+ * @size: sizeof bytes to be used
+ *
+ * Return: pointer to the allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *str;
+
+	str = malloc(nmemb * size);
+	if (str == NULL)
+		return (NULL);
+
+	return (str);
+}
