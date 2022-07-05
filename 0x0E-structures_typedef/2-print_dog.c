adfbc3e0fa7080c5ff41473ceee1364ae246c483
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-07-05 04:34:20 +0100

    .

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,7 +16,7 @@ void print_dog(struct dog *d)
 		else
 			printf("Name: %s\n", d->name);
 
-		if (d->age == NULL)
+		if (!(d->age))
 			printf("Age: (nil)\n");
 		else
 			printf("Age: %f\n", d->age);
@@ -25,4 +25,5 @@ void print_dog(struct dog *d)
 			printf("Owner: (nil)");
 		else
 			printf("Owner: %s\n", d->owner);
+	}
 }
