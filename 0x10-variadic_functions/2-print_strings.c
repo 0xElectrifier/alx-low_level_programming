553421058689eeae51ad8ccf375f7e484efdc94d
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-07-08 21:17:46 +0100

    "Fixed merge conflict"bash: line 1: :q: command not found
    ..

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,22 +1,5 @@
-#include "variadic_function.h"
+#include "variadic_functions.h"
 
-/**
- * va_arg_mod - function that checks if va_arg return is NULL
- * @arg_pointer: argument pointer
- * @type
- *
- * Return: string or nil if string is NULL
- */
-int *va_arg_mod(va_link arg_pointer)
-{
-	char p;
-
-	p = arg_pointer(arg_pointer, char);
-	if (p == NULL)
-		return ("nil");
-
-	return (p);
-}
 /**
  * print_strings - function that prints strings, followed by a new line
  * @separator: string to be printed between the strings
@@ -34,10 +17,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(arg_pointer, char);
+
 		if (str = NULL)
-		printf("s", "nil");
+			printf("s", "nil");
 		else
 			printf("s", str);
+
 		if ( i != (n - 1) && separator != NULL)
 			printf("s", separator);
 	}
