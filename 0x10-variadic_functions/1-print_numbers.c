7368be6694ec96357886bea553824579066bc663
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-07-08 19:51:51 +0100

    Omo

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function that prints numbers, followed by a new line
+ * @separator: string to be printed  between numbers
+ * @n: number of integers passed to the function
+ *
+ * Return: nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list arg_pointer;
+
+	va_start(arg_pointer, n);
+
+	for (i = 0; i <= n; i++)
+	{
+		printf("%d", va_arg(arg_pointer, unsigned int));
+		printf("%s", separator);
+		if (i == n)
+			break;
+	}
+	printf("\n");
+
+	va_end(arg_pointer);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,13 @@
 #ifndef _VARIADIC_FUNCTION
 #define _VARIADIC_FUNCTION
 
+#include <stdio.h>
 #include <stdarg.h>
 
 int _putchar(char n);
 
 int sum_them_all(const unsigned int n, ...);
+
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif
