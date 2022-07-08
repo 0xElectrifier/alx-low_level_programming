8cd584783a9c42c6fb8503d18ad4128332bd08cd
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-07-08 21:06:48 +0100

    Add

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,40 @@
+#include "variadic_function.h"
+
+/**
+ * va_arg_mod - function that checks if va_arg return is NULL
+ * @arg_pointer: argument pointer
+ * @type
+ *
+ * Return: string or nil if string is NULL
+ */
+int *va_arg_mod(va_link arg_pointer)
+{
+	char p;
+
+	p = arg_pointer(arg_pointer, char);
+	if (p == NULL)
+		return ("nil");
+
+	return (p);
+}
+/**
+ * print_strings - function that prints strings, followed by a new line
+ * @separator: string to be printed between the strings
+ * @n: number of strings passed to the function
+ *
+ * Return: nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	int i;
+	va_link arg_pointer;
+
+	va_start(arg_pointer, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("s", va_arg_mod(arg_pointer, char));
+		if ( i != (n - 1) && separator != NULL)
+			printf("s", separator);
+	}
+}
