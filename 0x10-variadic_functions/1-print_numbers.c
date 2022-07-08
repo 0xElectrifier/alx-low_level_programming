4a42a16759b6b5e5d6c62fbe4dda6a1b5810c96a
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-07-08 20:12:34 +0100

    Modified separator == NULL

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,18 +12,18 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list arg_pointer;
 
-	if (separator == NULL || n == 0)
-		return;
-
 	va_start(arg_pointer, n);
 
-	for (i = 0; i < n; i++)
+	if (separator != NULL && n != 0)
 	{
-		printf("%d", va_arg(arg_pointer, unsigned int));
-		if (i != (n - 1))
-			printf("%s", separator);
-	}
-	printf("\n");
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(arg_pointer, unsigned int));
+			if (i != (n - 1))
+				printf("%s", separator);
+		}
+		printf("\n");
 
-	va_end(arg_pointer);
+		va_end(arg_pointer);
+	}
 }
