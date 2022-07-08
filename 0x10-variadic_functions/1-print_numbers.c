32e03821d0d2b540e492e188bab90a429e08bda8
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-07-08 19:59:17 +0100

    Added Separator = NULL error fix

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,6 +12,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list arg_pointer;
 
+	if (separator == NULL || n == 0)
+		return;
+
 	va_start(arg_pointer, n);
 
 	for (i = 0; i < n; i++)
