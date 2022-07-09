891d483f240095b91f15e353be570a71065016e2
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-07-09 04:15:34 +0100

    First print-all push

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -12,5 +12,26 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 
 void print_strings(const char *separator, const unsigned int n, ...);
 
+/**
+ * struct print_arr - struct that contains a char and pointer function
+ * @s: data type specifier
+ * @print_func: pointer function that prints a specific data type input
+ *
+ */
+typedef struct
+{
+	char symbol;
+	void (*print)(va_list arg);
+} printer;
+
+void print_all(const char *const format, ...);
+
+void print_char(va_list arg);
+
+void print_int(va_list arg);
+
+void print_float(va_list arg);
+
+void print_string(va_list arg);
 
 #endif
