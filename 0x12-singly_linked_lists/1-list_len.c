6e4dcd11c9066de61889bd3856a6a1cd36382c65
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-07-16 14:12:39 +0100

    Created list length function

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,19 +1,20 @@
 #include "lists.h"
-/**
- * list_len - returns then number of elements in a list.
- * @h: singly linked list.
- * Return: number of elements in the list.
- */
 
+/**
+ * list_len - function that returns the number of elements in a linked list
+ * @h: pointer to the 'head'/first element of the linked list
+ *
+ * Return: number of elements in the linked list
+ */
 size_t list_len(const list_t *h)
 {
-	size_t nelem;
+	unsigned int node_count = 0;
 
-	nelem = 0;
 	while (h != NULL)
 	{
+		node_count++;
 		h = h->next;
-		nelem++;
 	}
-	return (nelem);
+
+	return (node_count);
 }
