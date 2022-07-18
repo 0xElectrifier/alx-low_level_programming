9370129028634c1ec3b9c07c435ee2e302d8283d
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-07-19 00:25:17 +0100

    Created print_listint()

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list
+ * @h: head of pointer to list to be printed
+ *
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	unsigned int node_count;
+	const listint_t *copy = h;
+
+	while(copy != NULL)
+	{
+		printf("%d\n", copy->n);
+		copy = copy->next;
+
+		node_count++;
+	}
+	return (node_count);
+}
