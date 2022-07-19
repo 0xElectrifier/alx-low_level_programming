eff61845a690da0d9f04f99829078f003b603226
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-07-19 20:22:05 +0100

    Updated the while loop statement

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,20 +1,21 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - returns the nth node of a linked list
- * @head: head of a list.
- * @index: index of the node.
+ * get_nodeint_at_index - function that returns the nth node of a listint_t
+ * linked list
+ * @head: Pointer to head of list
+ * @index: the index of the node to be returned starting at 0
  *
- * Return: nth node. If node does not exist, returns NULL.
+ * Return: nth node
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i;
+	unsigned int node_count = 0;
 
-	for (i = 0; i < index && head != NULL; i++)
+	while (node_count < index && head != NULL)
 	{
 		head = head->next;
+		node_count++;
 	}
-
 	return (head);
 }
