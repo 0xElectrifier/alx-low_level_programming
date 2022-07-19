002c32f71f16fd0ac544eb38674bd7dd94494323
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-07-19 22:06:04 +0100

    More mod

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		copy = copy->next;
 		node_count++;
 	}
-	if (copy == NULL || copy->next == NULL)
+	if (copy == NULL)
 		return (NULL);
 	else
 	{
