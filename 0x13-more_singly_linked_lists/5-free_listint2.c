c3c233f5d671654f14330638dddc56133c5a1c4f
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-07-19 09:20:22 +0100

    Free list and set head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a listint_t list
+ * @head: pointer to pointer to head of list
+ *
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp, *copy;
+
+	copy = *head;
+	while (copy != NULL)
+	{
+		temp = copy;
+		copy = copy->next;
+		free(temp):
+	}
+	*head = NULL;
+}
