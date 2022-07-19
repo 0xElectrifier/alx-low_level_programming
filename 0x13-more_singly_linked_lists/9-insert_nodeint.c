d9f595e13fa482de0c79aca97fb731fb3ac57e7d
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-07-19 21:30:31 +0100

    Modified **head = NULL error

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,6 +14,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int node_count;
 	listint_t *temp, *copy, *new;
 
+	if (head == NULL)
+		return (NULL);
+
 	copy = *head;
 	while (node_count < (idx - 1) && (copy != NULL))
 	{
