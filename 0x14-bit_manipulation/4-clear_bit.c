dfbf14f1cfd2a835571eab382c774f80944d01c9
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-07-22 16:39:15 +0100

    Created function to set bit @index to 0

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,24 +1,32 @@
 #include "main.h"
 
 /**
- * clear_bit - sets the value of a bit to 0.
- * at a given index.
- * @n: pointer of an unsigned long int.
- * @index: index of the bit.
+ * clear_bit - function that sets the value of a bit to 0 at a given index
+ * @n: pointer to integer to be set
+ * @index: index of n to be set
  *
- * Return: 1 if it worked, -1 if it didn't.
+ * Return: 1 if it worked, or -1 if an error occurred
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int m;
+	ULInt i, nt;
 
+	i = 0;
+	nt = 0;
 	if (index > 63)
 		return (-1);
 
-	m = 1 << index;
+	while (i < 64)
+	{
+		if (i == index)
+			nt += (0 << i);
+		else
+			nt += ((*n & 1) << i);
 
-	if (*n & m)
-		*n ^= m;
+		i++;
+		*n >>= 1;
+	}
+	*n = nt;
 
 	return (1);
 }
