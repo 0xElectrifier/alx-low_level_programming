ac137f7b9947ba7e50a4b82a135e0652949e92a5
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-07-22 15:46:55 +0100

    Created function to set bit at index

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,22 +1,34 @@
 #include "main.h"
 
 /**
- * set_bit - sets the value of a bit to 1.
- * at a given index.
- * @n: pointer of an unsigned long int.
- * @index: index of the bit.
+ * set_bit - function that sets the value of a bit to 1 at a given index
+ * @n: pointer to integer whose bit will be set
+ * @index: index of bit to set
  *
- * Return: 1 if it worked, -1 if it didn't.
+ * Return: 1 if it worked or -1 if an error occurred
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int m;
+	ULInt nt, i, bi;
+
+	nt = 0;
+	i = 0;
+	bi = 1;
 
 	if (index > 63)
 		return (-1);
 
-	m = 1 << index;
-	*n = (*n | m);
+	while (i < 64)
+	{
+		if (i == index)
+			nt += (1 * bi);
+		nt += ((*n & 1) * bi);
+
+		*n >>= 1;
+		bi <<= 1;
+		i++;
+	}
+	*n = nt;
 
 	return (1);
 }
