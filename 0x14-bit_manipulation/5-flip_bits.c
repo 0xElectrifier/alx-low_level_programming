51bfa245343563b2547c4e7f423059a56ae672db
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-07-22 17:36:32 +0100

    Created function that checks how bits will be flipped to get another number

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,22 +1,26 @@
 #include "main.h"
 
 /**
- * flip_bits - returns the number of bits you would
- * need to flip to get from one number to another
- * @n: number one.
- * @m: number two.
+ * flip_bits - function that returns the number of bits you would want to flip
+ * to get from one number to another
+ * @n: first integer
+ * @m: second integer
  *
- * Return: number of bits.
+ * Return: the number of flipped bits, sum
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int nbits;
+	unsigned int i, sum;
 
-	for (nbits = 0; n || m; n >>= 1, m >>= 1)
+	i = 0;
+	sum = 0;
+	while (i < 64)
 	{
 		if ((n & 1) != (m & 1))
-			nbits++;
+			sum++;
+		i++;
+		n >>= 1;
+		m >>= 1;
 	}
-
-	return (nbits);
+	return (sum);
 }
