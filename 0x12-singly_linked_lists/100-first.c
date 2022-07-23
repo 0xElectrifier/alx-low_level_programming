61885fed61ef8d5196d0bf2b2e406bb59f38eef1
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-07-23 17:14:58 +0100

    Added new line

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,7 +9,7 @@
 int __attribute__((constructor)) bmain(void)
 {
 	printf("You're beat! and yet, you must allow,\\nI bore my house ");
-	printf("upon my back!\\n");
+	printf("upon my back!\\n\n");
 
 	return (0);
 }
