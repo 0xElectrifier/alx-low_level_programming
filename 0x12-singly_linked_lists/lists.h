c35cd5cd35f2bbaedb44cdf3e6a1697b0107c699
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-07-23 16:36:37 +0100

    Created and added free_list() prototype

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -35,6 +35,6 @@ list_t *add_node_end(list_t **head, const char *str);
 
 list_t *add_node_e(list_t *head, const char *str);
 
-void *free_list(list_t *head);
+void free_list(list_t *head);
 
 #endif
