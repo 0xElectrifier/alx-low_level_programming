afa6afe7b09ff5947cd3a8dfad6de03fd8f2a915
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-07-26 17:02:27 +0100

    Resolved error @text_content == NULL

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,24 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - Function that returns the length of a string
- * @s: Takes pointer s of type char as parameter
- *
- * Return: 0 always
- */
-int _strlen(char *s)
-{
-	int i;
-
-	i = 0;
-	while (s[i] && s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
-/**
  * create_file - function that creates a file
  * @filename: name of file to be created
  * @text_content: a NULL terminated string to write to the file
@@ -27,19 +9,24 @@ int _strlen(char *s)
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, write_ret, buf_len;
+	int fd, write_ret, text_len;
 
 	if (filename == NULL)
 		return (-1);
-	buf_len = _strlen(text_content);
+	if (text_content == NULL)
+		text_content = "";
+
+	for (text_len = 0; text_content[text_len] != '\0'; text_len++)
+		continue;
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	write_ret = write(fd, text_content, buf_len);
+	write_ret = write(fd, text_content, text_len);
 	if (write_ret == -1)
 		return (-1);
 
 	close(fd);
+
 	return (1);
 }
