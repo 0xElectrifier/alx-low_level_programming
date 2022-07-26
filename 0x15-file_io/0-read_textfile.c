e67eced6f2a4da2bfe3eafdea8f6d89c5eec97a2
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-07-26 13:17:22 +0100

    Rectified write_ret() error

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,36 +1,40 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints the letters
- * @filename: filename.
- * @letters: numbers of letters printed.
+ * read_textfile - function that reads a text file and prints it to the POSIX
+ * standard output
+ * @filename: path to file
+ * @letters: number of letters it should read and print
  *
- * Return: numbers of letters printed. It fails, returns 0.
+ * Return: the actual number of letters it could read and print or 0 if file
+ * cannot be opened
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t nrd, nwr;
+	ssize_t fd;
+	int read_ret, write_ret;
 	char *buf;
 
-	if (!filename)
+	if (filename == NULL || letters == 0)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
-
 	if (fd == -1)
 		return (0);
-
-	buf = malloc(sizeof(char) * (letters));
-	if (!buf)
+	read_ret = read(fd, buf, letters);
+	if (read_ret == -1)
 		return (0);
 
-	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
-
-	close(fd);
+	write_ret = write(STDOUT_FILENO, buf, read_ret);
+	if (write_ret == -1)
+		return (0);
 
 	free(buf);
+	close(fd);
 
-	return (nwr);
+	return (write_ret);
 }
