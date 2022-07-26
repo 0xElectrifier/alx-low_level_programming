679911de806eb923826f6375d7727bb48347fca7
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-07-26 16:42:33 +0100

    Created a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,45 @@
 #include "main.h"
 
 /**
- * create_file - creates a file
- * @filename: filename.
- * @text_content: content writed in the file.
+ * _strlen - Function that returns the length of a string
+ * @s: Takes pointer s of type char as parameter
  *
- * Return: 1 if it success. -1 if it fails.
+ * Return: 0 always
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * create_file - function that creates a file
+ * @filename: name of file to be created
+ * @text_content: a NULL terminated string to write to the file
+ *
+ * Return: 1 on success and -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int nletters;
-	int rwr;
+	int fd, write_ret, buf_len;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
+	buf_len = _strlen(text_content);
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
 	if (fd == -1)
 		return (-1);
-
-	if (!text_content)
-		text_content = "";
-
-	for (nletters = 0; text_content[nletters]; nletters++)
-		;
-
-	rwr = write(fd, text_content, nletters);
-
-	if (rwr == -1)
+	write_ret = write(fd, text_content, buf_len);
+	if (write_ret == -1)
 		return (-1);
 
 	close(fd);
-
 	return (1);
 }
