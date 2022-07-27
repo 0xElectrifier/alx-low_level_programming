c8dc6fe94d69d0377e0e5fb3bfa125f683fded0f
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-07-27 02:42:48 +0100

    More mod

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,22 +9,20 @@
  *
  * Return: 0
  */
-int print_err(int file, int flag, char *sw, char **av)
+void print_err(int file, int flag, char *sw, char **av)
 {
 	(void) av;
 
 	if (flag == CLOSE)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't %s fd %d\n", sw, file);
-		return (flag);
+		exit(flag);
 	}
 	else
 	{
 		dprintf(STDERR_FILENO, "Error: Can't %s file %s\n", sw, av[file]);
-		return (flag);
+		exit(flag);
 	}
-
-	return (0);
 }
 
 /**
@@ -48,33 +46,28 @@ int main(int ac, char **av)
 
 	file_from = open(av[1], O_RDONLY);
 	if (file_from == -1)
-		exit(print_err(1, READ, READ_T, av));
+		print_err(1, READ, READ_T, av);
 	file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	if (file_to == -1)
-		exit(print_err(2, WRITE, WRITE_T, av));
+		print_err(2, WRITE, WRITE_T, av);
 	buf_size = 1024;
 	while (buf_size == 1024)
 	{
 		buf_size = read(file_from, buf, 1024);
 		if (buf_size == -1)
-			exit(print_err(1, READ, READ_T, av));
+			print_err(1, READ, READ_T, av);
 		file_t = write(file_to, buf, buf_size);
 		if (file_t == -1)
-			exit(print_err(2, WRITE, WRITE_T, av));
+			print_err(2, WRITE, WRITE_T, av);
 	}
 
 	c_f1 = close(file_to);
 	if (c_f1 == -1)
-	{
 		print_err(file_to, CLOSE, CLOSE_T, av);
-		exit(100);
-	}
+
 	c_f2 = close(file_from);
 	if (c_f2 == -1)
-	{
 		print_err(file_from, CLOSE, CLOSE_T, av);
-		exit(100);
-	}
 
 	return (0);
 }
