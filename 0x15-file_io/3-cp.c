92fac01402f78649594c5d11fcb99415927769cc
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-07-27 01:41:04 +0100

    Created funcion to copy a file into another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,30 @@
 #include "main.h"
 
 /**
+ * print_err - function that prints to stderr
+ * @file_from: file descriptor to the original file
+ * @file_to: file descriptor to the second file (destination)
+ * @av:pointer to array of pointers to the commands passed on the terminal
+ *
+ * Return: 0
+ */
+int print_err(int file, int flag, char *sw, char **av)
+{
+	(void) av;
+
+	if (flag == CLOSE)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't %s fd %d\n", sw, file);
+		exit(flag);
+	}
+
+	dprintf(STDERR_FILENO, "Error: Can't %s file %s\n", sw, av[file]);
+	exit(flag);
+
+	return (0);
+}
+
+/**
  * main - duplicate of the linux 'cp' command that copies contents of a file
  * into another file
  * @ac: number of arguements passed on the terminal
@@ -10,10 +34,45 @@
  */
 int main(int ac, char **av)
 {
+	int file_from, file_to, file_f, file_t, buf_size, c_f1, c_f2;
+	char buf[1024];
+
 	if (ac != 3)
 	{
-		fprintf((FILE) STDERR_FILENO, "Usage: %s file_from file_to\n", av[0]);
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", av[0]);
 		exit(97);
 	}
-	return (1);
+
+	file_from = open(av[1], O_RDONLY);
+	if (file_from == -1)
+		print_err(1, READ, READ_T, av);
+	file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	if (file_to == -1)
+		print_err(2, WRITE, WRITE_T, av);
+
+	buf_size = 1024;
+	while (buf_size == 1024)
+	{
+		buf_size = read(file_from, buf, 1024);
+		if (buf_size == -1)
+			print_err(1, READ, READ_T, av);
+		file_t = write(file_to, buf, buf_size);
+		if (file_t == -1)
+			print_err(2, WRITE, WRITE_T, av);
+	}
+
+	c_f1 = close(file_to);
+	if (c_f1 == -1)
+	{
+		print_err(file_to, CLOSE, CLOSE_T, av);
+		exit(100);
+	}
+	c_f2 = close(file_from);
+	if (c_f2 == -1)
+	{
+		print_err(file_from, CLOSE, CLOSE_T, av);
+		exit(100);
+	}
+
+	return (0);
 }
