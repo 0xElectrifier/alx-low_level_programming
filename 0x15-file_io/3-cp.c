7b9f285766ace3136b2c45f61f8c4f92121d0be2
Akacha-Ubuntu <samchijioke16@gmail.com>
2022-07-27 02:04:35 +0100

    Adjusted the exit status of the code

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,12 +16,12 @@ int print_err(int file, int flag, char *sw, char **av)
 	if (flag == CLOSE)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't %s fd %d\n", sw, file);
-		exit(flag);
+		return(flag);
 	}
 	else
 	{
 		dprintf(STDERR_FILENO, "Error: Can't %s file %s\n", sw, av[file]);
-		exit(flag);
+		return(flag);
 	}
 
 	return (0);
@@ -48,19 +48,19 @@ int main(int ac, char **av)
 
 	file_from = open(av[1], O_RDONLY);
 	if (file_from == -1)
-		print_err(1, READ, READ_T, av);
+		exit(print_err(1, READ, READ_T, av));
 	file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	if (file_to == -1)
-		print_err(2, WRITE, WRITE_T, av);
+		exit(print_err(2, WRITE, WRITE_T, av));
 	buf_size = 1024;
 	while (buf_size == 1024)
 	{
 		buf_size = read(file_from, buf, 1024);
 		if (buf_size == -1)
-			print_err(1, READ, READ_T, av);
+			exit(print_err(1, READ, READ_T, av));
 		file_t = write(file_to, buf, buf_size);
 		if (file_t == -1)
-			print_err(2, WRITE, WRITE_T, av);
+			exit(print_err(2, WRITE, WRITE_T, av));
 	}
 
 	c_f1 = close(file_to);
