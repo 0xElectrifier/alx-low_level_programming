45442a013e8431039dde0df006ddc79bd2ce7dab
Akacha Ubuntu <samchijioke16@gmail.com>
2022-09-06 04:47:11 +0100

    Created program that calculates and returns the sum of all data (n) in a dlistint_t list

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,28 +1,19 @@
 #include "lists.h"
 
 /**
- * sum_dlistint - returns the sum of all the data (n)
- * of a doubly linked list
+ * sum_dlistint - calculates the sum of all data of a dlistint_t linked list
+ * @head: pointer to head of list
  *
- * @head: head of the list
- * Return: sum of the data
+ * Return: sum of all ->n in list
  */
 int sum_dlistint(dlistint_t *head)
 {
-	int sum;
+	int sum = 0;
 
-	sum = 0;
-
-	if (head != NULL)
+	while (head != NULL)
 	{
-		while (head->prev != NULL)
-			head = head->prev;
-
-		while (head != NULL)
-		{
-			sum += head->n;
-			head = head->next;
-		}
+		sum += head->n;
+		head = head->next;
 	}
 
 	return (sum);
