bbdd3e916fd1e3b8beb58a04646ecc243690fe9e
Akacha Ubuntu <samchijioke16@gmail.com>
2022-09-06 03:05:09 +0100

    Created function that finds length of list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,29 +1,19 @@
 #include "lists.h"
 
 /**
- * dlistint_len - returns the number of elements in
- * a double linked list
+ * dlistint_len - Returns the number of elements of a linked dlistint_t list
+ * @h: pointer to head of node
  *
- * @h: head of the list
- * Return: the number of nodes
+ * Return: number of elements in list
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	int count;
-
-	count = 0;
-
-	if (h == NULL)
-		return (count);
-
-	while (h->prev != NULL)
-		h = h->prev;
+	int count = 0;
 
 	while (h != NULL)
 	{
 		count++;
 		h = h->next;
 	}
-
 	return (count);
 }
