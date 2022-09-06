17e50cb398d26bbba5636d6d98d2e2bc56ded9a5
Akacha Ubuntu <samchijioke16@gmail.com>
2022-09-06 23:27:47 +0100

    Handled idx == 0

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,16 +13,21 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 	dlistint_t *temp, *new;
 
+	if (h == NULL)
+		return (NULL);
+
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
 	new->prev = NULL;
-	new->next = NULL;
 
 	temp = *h;
-	if (temp == NULL && idx == 0)
+	if (idx == 0)
 	{
+		new->next = *h;
+		if (*h != NULL)
+			(*h)->prev = new;
 		*h = new;
 		return (new);
 	}
