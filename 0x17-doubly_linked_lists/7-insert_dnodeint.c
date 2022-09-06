9276dd4523bc5f7e3342b40af89f27520f28af87
Akacha Ubuntu <samchijioke16@gmail.com>
2022-09-07 00:51:17 +0100

    Free'd new node on failure to add node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -38,7 +38,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		i++;
 	}
 	if (temp == NULL)
+	{
+		free(new);
 		return (NULL);
+	}
 
 	new->next = temp->next;
 	new->prev = temp;
