60dbb5197c1a1e7f260a56af8ca66366b7a37c7a
Akacha Ubuntu <samchijioke16@gmail.com>
2022-09-07 17:18:33 +0100

    Resolved betty error

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -2,7 +2,7 @@
 
 /**
  * delete_dnodeint_at_index - deletes the node at index of a dlistint_t
- * 				linked list
+ *				linked list
  * @head: pointer to pointer to head of list
  * @index: position of item in list to be deleted
  *
