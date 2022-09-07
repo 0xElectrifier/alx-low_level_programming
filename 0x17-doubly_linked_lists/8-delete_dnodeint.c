0a88f4b0f025d09cd02e6c206c107003c30ff52f
Akacha Ubuntu <samchijioke16@gmail.com>
2022-09-07 17:12:56 +0100

    Created function to delete data at index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,52 +1,39 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - deletes the node at index of a
- * dlistint_t linked list
+ * delete_dnodeint_at_index - deletes the node at index of a dlistint_t
+ * 				linked list
+ * @head: pointer to pointer to head of list
+ * @index: position of item in list to be deleted
  *
- * @head: head of the list
- * @index: index of the new node
- * Return: 1 if it succeeded, -1 if it failed
+ * Return: 1 if succeeded, -1 if it failed
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *h1;
-	dlistint_t *h2;
-	unsigned int i;
+	dlistint_t *temp, *h = *head;
+	unsigned int i = 0;
 
-	h1 = *head;
+	if (head == NULL)
+		return (-1);
 
-	if (h1 != NULL)
-		while (h1->prev != NULL)
-			h1 = h1->prev;
-
-	i = 0;
-
-	while (h1 != NULL)
+	while (h != NULL && i < index)
 	{
-		if (i == index)
-		{
-			if (i == 0)
-			{
-				*head = h1->next;
-				if (*head != NULL)
-					(*head)->prev = NULL;
-			}
-			else
-			{
-				h2->next = h1->next;
-
-				if (h1->next != NULL)
-					h1->next->prev = h2;
-			}
-
-			free(h1);
-			return (1);
-		}
-		h2 = h1;
-		h1 = h1->next;
+		h = h->next;
 		i++;
 	}
+	if (h == NULL)
+		return (-1);
 
-	return (-1);
+	temp = h;
+	if (index == 0)
+		*head = (temp)->next;
+	else
+		(temp->prev)->next = temp->next;
+
+	if (temp->next != NULL)
+		(temp->next)->prev = temp->prev;
+
+	free(temp);
+
+	return (1);
 }
