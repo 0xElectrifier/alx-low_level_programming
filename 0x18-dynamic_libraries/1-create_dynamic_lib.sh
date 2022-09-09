223c17d285aa1e70d3c38d54acc9f1888b5c17bb
Akacha Ubuntu <samchijioke16@gmail.com>
2022-09-09 01:46:00 +0100

    Made some mod

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,2 @@
-#!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -fPIC -c *.c
-gcc -shared -Wl,-soname,liball.so -o liball.so *.o
+#!/usr/bin/bash
+gcc -c -fPIC *.c && gcc -L -shared *.o -o liball.so
