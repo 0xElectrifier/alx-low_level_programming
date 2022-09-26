68473678a7dc44b744b6095915955c58d4cadf62
Akacha Ubuntu <samchijioke16@gmail.com>
2022-09-26 20:21:10 +0100

    Wrote my own strdup function

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,32 @@
 #include "hash_tables.h"
 
 /**
+ * _strdup - function that returns a pointer to a newly allocated space in
+ * memory, which contains a copy of the string given as a parameter
+ * @str: str to be referenced
+ *
+ * Return: pointer to the newly allocated space
+ */
+char *_strdup(const char *str)
+{
+	unsigned int i, str_len;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+
+        while (str[str_len] != '\0')
+		str_len++;
+        s = malloc(str_len * sizeof(char) + 1);
+        if (s == NULL)
+                return (NULL);
+
+        for (i = 0; i < str_len; i++)
+                s[i] = str[i];
+
+        return (s);
+}
+/**
  * hash_table_set - Adds an element to the hash table
  * @ht: the hash table
  * @key: the key
@@ -22,8 +48,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 		return (0);
-	new->key = strdup(key);
-	new->value = strdup(value);
+	new->key = _strdup(key);
+	new->value = _strdup(value);
 	new->next = (ht->array)[index];
 
 	(ht->array)[index] = new;
