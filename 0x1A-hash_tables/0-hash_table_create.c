53bc11c96c69c4c49686555036bb0c92dc15968c
Akacha Ubuntu <samchijioke16@gmail.com>
2022-09-26 11:25:44 +0100

    Declared  variable and resolved wrong memory allocation size

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,12 +9,13 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	hash_node_t **arr;
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 
-	arr = malloc(sizeof(hash_node_t) * size);
+	arr = malloc(sizeof(hash_node_t *) * size);
 	if (arr == NULL)
 		return (NULL);
 
