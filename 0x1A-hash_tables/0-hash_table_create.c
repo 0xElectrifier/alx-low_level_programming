9e876589cf0585cce55e978a3ab7675bcb97d488
Akacha Ubuntu <samchijioke16@gmail.com>
2022-09-26 11:09:57 +0100

    Created a function that creates and returns an empty hash array

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,30 +1,25 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table
- * @size: size of the table
- * Return: hash table on success, NULL on failure.
+ * hash_table_create - Creates a hash table
+ * @size: size of the array to be allocated
+ *
+ * Return: pointer to the a newly created hash table, or NULL on failure
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	hast_table_t *table;
 
-	hash_table_t *hash_table;
-
-	if (size == 0)
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
 		return (NULL);
 
-	hash_table = malloc(sizeof(hash_table_t));
-
-	if (hash_table == NULL)
+	arr = malloc(sizeof(hash_node_t) * size);
+	if (arr == NULL)
 		return (NULL);
 
-	hash_table->size = size;
-	hash_table->array = calloc(size, sizeof(hash_node_t *));
-	if (hash_table->array == NULL)
-	{
-		free(hash_table);
-		return (NULL);
-	}
-	return (hash_table);
+	table->size = size;
+	table->array = array;
 
+	return (table);
 }
