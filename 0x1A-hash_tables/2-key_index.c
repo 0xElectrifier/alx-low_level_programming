cd678f8493ca37e7fba1ab104c1a4e13c3fdb231
Akacha Ubuntu <samchijioke16@gmail.com>
2022-09-26 16:31:16 +0100

    Created function to return an index of a key in the array

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,14 +1,19 @@
 #include "hash_tables.h"
 
 /**
- * key_index - returns the index of a key
- * @key: key
- * @size: size of the array of the hash table
- * Return: index
+ * key_index - computes the index of a key using the djb2 algorithm
+ * @key: the key
+ * @size: size of the array
+ *
+ * Return: the index index of @key in the array
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int index = hash_djb2(key) % size;
+	unsigned long int hash;
 
-	return (index);
+	if (key == NULL)
+		return (-1);
+	hash = hash_djb2(key);
+
+	return (hash % size);
 }
