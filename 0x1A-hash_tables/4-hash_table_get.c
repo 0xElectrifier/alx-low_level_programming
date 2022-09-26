949bf98a3d15ce4eac6eccb37517ad5c651d895c
Akacha Ubuntu <samchijioke16@gmail.com>
2022-09-27 00:30:13 +0100

    Created search function for the hash table

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,28 +1,29 @@
 #include "hash_tables.h"
+
 /**
- * hash_table_get - retrieves a value associated with a key.
- * @ht: the hash table you want to look into
- * @key: is the key you are looking for
- * Return: value associated with key if found, NULL if failed
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: the hash table
+ * @key: the key of the item to be retrieved
+ *
+ * Return: the value associated with @key, or `NULL` if key can't be found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	unsigned long int table_size, index;
+	hash_node_t *temp;
 
-hash_node_t *node;
-unsigned long int index;
+	if (ht == NULL)
+		return (NULL);
 
-if (ht == NULL)
+	table_size = ht->size;
+	index = key_index(key, table_size);
+	temp = ht->array[index];
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return (key->value);
+		temp = temp->next;
+	}
+
 	return (NULL);
-
-index = key_index((const unsigned char *) key, ht->size);
-node = ht->array[index];
-
-while (node != NULL)
-{
-	if (strcmp(node->key, key) == 0)
-		return (node->value);
-
-	node = node->next;
-}
-return (NULL);
 }
